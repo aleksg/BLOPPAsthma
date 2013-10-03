c805ff69a201a65b94acd9b7bc636e29d666eb32
Esben Aarseth <esbena@stud.ntnu.no>
2013-10-03 11:24:51 +0200

    yay

---
+++ BloppAstma/src/com/blopp/bloppasthma/utils/AvailableMedicines.java
@@ -0,0 +1,44 @@
+package com.blopp.bloppasthma.utils;
+import java.util.HashMap;
+
+/**
+ * This is a helper class to get the available medicines in the database.
+ * Thus, medcinesMap must always reflect the exact fields in the database. If not,
+ * medicines will not appear in any lists.
+ */
+public class AvailableMedicines
+{
+	public HashMap<String, Integer> medicinesMap;
+	
+	public AvailableMedicines()
+	{
+		medicinesMap = new HashMap<String, Integer>();
+		medicinesMap.put("Flutide", 1);
+		medicinesMap.put("Ventoline", 2);
+		medicinesMap.put("Seretide", 3);
+	}
+	
+	public int getMedicineByName(String name)
+	{
+		for (String s : medicinesMap.keySet())
+		{
+			if(s.equalsIgnoreCase(name))
+			{
+				return medicinesMap.get(s);
+			}
+		}
+		throw new IllegalArgumentException("The name of this medicine does not exist");
+	}
+	public String getMedicineById(int id)
+	{
+		for (String s : medicinesMap.keySet())
+		{
+			if(id==medicinesMap.get(s))
+			{
+				return s;
+			}
+		}
+		throw new IllegalArgumentException("The id does not exist");
+	}
+}
+
