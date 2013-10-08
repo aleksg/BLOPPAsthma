0ab978cd125236c3cda9c36f1d8434abbfc223d2
Aleks Gisvold <gisvold@gmail.com>
2013-10-08 15:01:39 +0200

    changes to cameractivity

--- BloppAstma/src/com/blopp/bloppasthma/activities/CameraActivity.java
+++ BloppAstma/src/com/blopp/bloppasthma/activities/CameraActivity.java
@@ -6,34 +6,60 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class CameraActivity extends Activity {
+public class CameraActivity extends Activity implements OnClickListener {
     private static final int CAMERA_REQUEST = 1888; 
     private ImageView imageView;
+    
+    private Button photoButton, savereturnButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camerapreview);
         this.imageView = (ImageView)this.findViewById(R.id.imageView1);
-        Button photoButton = (Button) this.findViewById(R.id.button1);
-        photoButton.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE); 
-                startActivityForResult(cameraIntent, CAMERA_REQUEST); 
-            }
-        });
+
+        setUpListeners();
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
+    private void setUpListeners() {
+    	setUpListenerForButton(savereturnButton, R.id.savereturnbutton);
+    	setUpListenerForButton(photoButton, R.id.photobutton);
+		
+	}
+
+	private void setUpListenerForButton(Button b, int id) {
+		b = (Button) findViewById(id);
+		if(b == null){
+			return;
+		}
+		b.setOnClickListener(this);
+		Log.d("MainMenu", "Set up listener for id:" + id);
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
         if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {  
             Bitmap photo = (Bitmap) data.getExtras().get("data"); 
             imageView.setImageBitmap(photo);
         }  
     } 
+    
+    
+    public void onClick(View v) {
+    	int vid = v.getId();
+    	Log.d("CamerActivity", "pressed button: " + v.getId());
+    	if	(vid == R.id.photobutton){
+    		Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE); 
+    		startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    	} else if (vid == R.id.savereturnbutton) {
+    		//TODO: Send image object back to activity
+    	}
+    	
+    }
 }
