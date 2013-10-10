936d2d56bf3e88a3acc71178ef77fbafed9e7b1f
Esben Aarseth <esbena@stud.ntnu.no>
2013-10-10 11:20:19 +0200

    fixed

--- BloppAstma/src/com/blopp/bloppasthma/activities/AddRewardActivity.java
+++ BloppAstma/src/com/blopp/bloppasthma/activities/AddRewardActivity.java
@@ -21,123 +21,105 @@ import com.blopp.bloppasthma.utils.TemporarilyImageStore;
 public class AddRewardActivity extends Activity
 {
 	private static String sharedPreferenceName = "RewardList";
-	
+
 	private static final String TAG = AddRewardActivity.class.getSimpleName();
 	private EditText descriptionText, starsText;
 	private Button findImageButton, saveRewardButton;
 	private CheckBox repeatRewardCheckbox;
 	private SavedRewards savedRewards;
 	private Bitmap selectedImage;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add_reward);
-		
+
 		savedRewards = new SavedRewards(getApplicationContext());
-		
-		descriptionText = (EditText)findViewById(R.id.descriptionText);
-		findImageButton = (Button)findViewById(R.id.findImageButton);
-		saveRewardButton = (Button)findViewById(R.id.saveRewardButton);
+
+		descriptionText = (EditText) findViewById(R.id.descriptionText);
+		findImageButton = (Button) findViewById(R.id.findImageButton);
+		saveRewardButton = (Button) findViewById(R.id.saveRewardButton);
 		repeatRewardCheckbox = (CheckBox) findViewById(R.id.repeatRewardCheckbox);
-		starsText = (EditText)findViewById(R.id.rewardCostText);
+		starsText = (EditText) findViewById(R.id.rewardCostText);
 		saveRewardButton.setOnClickListener(new OnClickListener()
-				{
-					@Override
-					public void onClick(View v)
-					{
-						save();
-						setResult(Activity.RESULT_OK, new Intent());
-						finish();
-					}
-				});
+		{
+			@Override
+			public void onClick(View v)
+			{
+				save();
+				setResult(Activity.RESULT_OK, new Intent());
+				finish();
+			}
+		});
 		findImageButton.setOnClickListener(new FindImageClickListener());
-		
+
 	}
-	
+
 	private void save()
 	{
 		savedRewards.saveReward(createReward());
-		TemporarilyImageStore store = new TemporarilyImageStore(getApplicationContext());
+		TemporarilyImageStore store = new TemporarilyImageStore(
+				getApplicationContext());
 		store.removeTemporarilyImage();
 	}
-	
+
 	private Reward createReward()
 	{
 		String desc = descriptionText.getText().toString();
 		int stars = Integer.parseInt(starsText.getText().toString());
 		boolean repeat = repeatRewardCheckbox.isSelected();
 		int currentMax = savedRewards.getMaximumIdentifier();
-		
-		Reward r = new Reward(currentMax)
-						.setDescription(desc)
-						.setStars(stars)
-						.setOrdered(false)
-						.setRepeat(repeat);	
 
-		if(selectedImage == null){
+		Reward r = new Reward(currentMax).setDescription(desc).setStars(stars)
+				.setOrdered(false).setRepeat(repeat);
+
+		if (selectedImage == null)
+		{
 
 			Log.d(TAG, "Selected image is null");
 
-			selectedImage = BitmapFactory.decodeResource(getResources(), R.drawable.book_small);
+			selectedImage = BitmapFactory.decodeResource(getResources(),
+					R.drawable.book_small);
 			r.setBitmap(selectedImage);
-		}else{
+		} else
+		{
 			Log.d(TAG, "Selected image was not null");
 			r.setBitmap(selectedImage);
 		}
 
 		return r;
-
 	}
+
 	private class FindImageClickListener implements OnClickListener
 	{
 		@Override
 		public void onClick(View v)
 		{
 
-			startActivityForResult(new Intent(AddRewardActivity.this, CameraActivity.class), RESULT_OK);
+			startActivityForResult(new Intent(AddRewardActivity.this,
+					CameraActivity.class), RESULT_OK);
 			Log.d(TAG, "Should find images now.");
 		}
-		
+
 	}
-	
-	private void activityStarter(Class<?> c)
-	{
-		Log.d(c.getSimpleName(), "activityStarter");
-		Intent intent = new Intent(AddRewardActivity.this, c);
-		startActivity(intent);
-	}
-	
-	
-	
+
 	@Override
-	protected void onResume() {
+	protected void onResume()
+	{
 		Log.d(TAG, "Resuming activity");
-		
+
 		super.onResume();
-		
-		TemporarilyImageStore store = new TemporarilyImageStore(getApplicationContext());
-		if(store.hasStoredAnImage())
+
+		TemporarilyImageStore store = new TemporarilyImageStore(
+				getApplicationContext());
+		if (store.hasStoredAnImage())
 		{
 			Log.d(TAG, "Found an image. WOHO!");
 			selectedImage = store.getByteImage().getImageAsBitmap();
 		}
-		
-	}
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data)
-	{
-		Log.d(TAG, "onActivityResult is called");
-		if(getIntent().hasExtra("image")){
-			byte[] image = getIntent().getExtras().getByteArray("image");
-			selectedImage = BitmapFactory.decodeByteArray(image, 0, image.length);
-		}else{
-			Log.d(TAG, "Had no extra");
-		}
+
 	}
 
 }
-
