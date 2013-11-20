20ddae46088a10930b164e68a546f9f2b6013b6d
Aleks Gisvold <gisvold@gmail.com>
2013-11-20 13:03:35 +0100

    commit fordi Esben sa det

--- BloppAPI/src/com/blopp/bloppapi/posters/TreatmenttPoster.java
+++ BloppAPI/src/com/blopp/bloppapi/posters/TreatmenttPoster.java
@@ -1,14 +1,16 @@
 package com.blopp.bloppapi.posters;
 
+import com.blopp.bloppapi.gsondeserializers.ChildCreditsDeserializer;
 import com.blopp.bloppapi.models.Child;
+import com.blopp.bloppapi.models.Treatment;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class CreditPoster extends BLOPPPoster<Child>
+public class TreatmenttPoster extends BLOPPPoster<Child>
 {
 	
-	private static final String phpPage = "";
-	public CreditPoster(String params)
+	private static final String phpPage = "register_medicine_taken?";
+	public TreatmenttPoster(String params)
 	{
 		super(phpPage, params);
 	}
@@ -23,7 +25,9 @@ public class CreditPoster extends BLOPPPoster<Child>
 		Gson gson = builder.create();
 		Child child = gson.fromJson(getReply(), Child.class);
 		System.out.println("Child id = " + child.getUid());
-		System.out.println("Total credits = " + child.getCredits());
+		System.out.println("MedicineID = " + child.getCredits());
+		System.out.println("Healthstate = " + child.getHealthState());
+		System.out.println("MedicalPlanDoseID = " + child.getMedicalPlanId());
 				
 		return child;
 	}
