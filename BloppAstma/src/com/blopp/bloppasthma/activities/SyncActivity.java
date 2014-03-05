30005745828980c9e6b53358a8afb73668031a79
Aleks Gisvold <gisvold@gmail.com>
2014-03-05 14:55:02 +0100

    made connection between menu and sync method

--- BloppAstma/src/com/blopp/bloppasthma/activities/SyncActivity.java
+++ BloppAstma/src/com/blopp/bloppasthma/activities/SyncActivity.java
@@ -1,8 +1,13 @@
 package com.blopp.bloppasthma.activities;
 
+import java.util.concurrent.ExecutionException;
+
 import com.blopp.bloppasthma.R;
+import com.blopp.bloppasthma.jsonposters.NewUserPoster;
+import com.blopp.bloppasthma.mockups.ChildIdService;
 
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -23,7 +28,18 @@ public class SyncActivity extends Activity
 	    // Handle item selection
 	    switch (item.getItemId()) {
 	        case R.id.sync:
-	            //sync(); TODO: Make connection with method for syncing
+	            NewUserPoster newPoster = new NewUserPoster(new ChildIdService(getApplicationContext()).getChildId() + "");
+	            newPoster.execute();
+			try {
+				String response = newPoster.get();
+				Log.d("SyncActivity", response);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	            return true;
 	        default:
 	            return super.onOptionsItemSelected(item);
